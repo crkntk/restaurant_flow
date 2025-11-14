17dab7b46dcf05779994ed3fad19b562e295dc98
crkntk <crkntk@gmail.com>
2025-11-14 00:16:48 -0800

    insert and remove

--- monitor.cpp
+++ monitor.cpp
@@ -3,7 +3,34 @@
 Monitor::Monitor(int genCapacity, int vipCapacity)
 {
     this->normalCapacity = genCapacity;
-    this->availVIPSeats = availVIPSeats;
+    this->VIPCapacity = vipCapacity;
     this->queueGenReq = 0;
     this->queueVipReq = 0;
+    pthread_cond_init(&this->seatsAvail, NULL);
+    pthread_cond_init(&this->unconsumedSeats, NULL);
+    pthread_cond_init(&this->VipSeatsAvail, NULL);
+    pthread_mutex_init(&this->mutex, NULL);
+}
+
+void Monitor::insert(RequestType request){
+    bool onlyItem;
+    pthread_mutex_lock(&mutex);
+    while(this->queueGenReq >= this->normalCapacity){
+        pthread_cond_wait(&seatsAvail,&this->mutex);
+    }
+    onlyItem = (queueGenReq == 0);
+    this->buffer.push(request);
+
+    if(onlyItem){
+        pthread_cond_signal(&unconsumedSeats);
+    }
+    pthread_mutex_unlock(&this->mutex);
+}
+RequestType Monitor::remove(){
+    RequestType request;
+    bool atCapacity;
+    pthread_mutex_lock(&mutex);
+    while(this-> queueGenReq == 0){
+        pthread_cond_wait(&unconsumedSeats, &this->mutex);
+    }
 }
--- monitor.h
+++ monitor.h
@@ -3,18 +3,20 @@
 #include "seating.h"
 #ifndef MONITOR_H
 #define MONITOR_H
-
+using namespace std;
 class Monitor
 {
     Monitor(int genCapacity, int vipCapacity);
     pthread_mutex_t mutex;
     pthread_cond_t VipSeatsAvail;
     pthread_cond_t seatsAvail;
+    pthread_cond_t unconsumedSeats;
     int normalCapacity;
     int VIPCapacity;
     int queueGenReq;
     int queueVipReq;
-    void insert(RequestType reqType);
-    int remove();
+    queue <RequestType> buffer;
+    void insert(RequestType request);
+    RequestType remove();
 };
 #endif
